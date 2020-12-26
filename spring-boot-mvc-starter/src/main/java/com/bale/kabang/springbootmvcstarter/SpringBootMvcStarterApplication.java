87d41555dd610774085ae6d809eccc5c886fd705
githong <githong@kakao.com>
2020-12-27 00:41:32 +0900

    initial commit

---
+++ spring-boot-mvc-starter/src/main/java/com/bale/kabang/springbootmvcstarter/SpringBootMvcStarterApplication.java
@@ -0,0 +1,13 @@
+package com.bale.kabang.springbootmvcstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcStarterApplication.class, args);
+	}
+
+}
